a0175ed6be3f87e6f67fdd986163be6a483ec96d
Harshad <harshadarvindh@gmail.com>
2024-03-30 17:22:38 -0400

    splashscreen

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
     gameButtons.forEach((button) => {
       const gameName = button.querySelector("img").alt.toLowerCase();
       if (gameName.includes(searchTerm)) {
-        button.style.display = "block";
+        button.style.display = "inline-block";
       } else {
         button.style.display = "none";
       }
