cfd59c3ae5fdb43d8d7220ff1ca7d946faa6ae6c
Harshad <harshadarvindh@gmail.com>
2024-03-30 15:01:26 -0400

    no

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const searchInput = document.getElementById("searchInput");
+  const gameContainer = document.getElementById("gameContainer");
+  const gameButtons = document.querySelectorAll(".Game_Button");
+
+  searchInput.addEventListener("input", function () {
+    const searchTerm = searchInput.value.trim().toLowerCase();
+
+    gameButtons.forEach((button) => {
+      const gameName = button.querySelector("img").alt.toLowerCase();
+      if (gameName.includes(searchTerm)) {
+        button.style.display = "block";
+      } else {
+        button.style.display = "none";
+      }
+    });
+  });
+});
