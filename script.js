32431fdceac5a558c9b7f779e744c3d89f8a5490
Harshad <harshadarvindh@gmail.com>
2024-04-01 19:54:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-window.addEventListener("load", function (
-) {
+document.addEventListener("DOMContentLoaded", function () {
+  const searchInput = document.getElementById("searchInput");
+  const gameContainer = document.getElementById("gameContainer");
+  const gameButtons = document.querySelectorAll(".Game_Button");
 
-if (!localStorage.getItem('redirected')) {
-        window.location.href = '/pages/educational_Portal.html';
-        localStorage.setItem('redirected', true);
-    }
+  searchInput.addEventListener("input", function () {
+    const searchTerm = searchInput.value.trim().toLowerCase();
 
-
-
-
-setTimeout(() => {
-        splashScreen.style.animation = "slidedown 0.9s ease-in-out forwards";
-
-        splashScreen.addEventListener("animationend", function (
-) {
-            splashScreen.style.display = "none";
-            mainContent.style.display = "block";
-        });
-    }, 1700);
+    gameButtons.forEach((button) => {
+      const gameName = button.querySelector("img").alt.toLowerCase();
+      if (gameName.includes(searchTerm)) {
+        button.style.display = "inline-block";
+      } else {
+        button.style.display = "none";
+      }
+    });
+  });
 });
+
+const splashDuration = 1700;
+
+setTimeout(function () {
+  const splashScreen = document.getElementById("splashScreen");
+  splashScreen.style.animation = "slidedown 0.9s ease-in-out forwards";
+
+  splashScreen.addEventListener("animationend", function () {
+    splashScreen.style.display = "none";
+    document.getElementById("mainContent").style.display = "block";
+  });
+}, splashDuration);
