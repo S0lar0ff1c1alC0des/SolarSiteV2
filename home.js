1d99d40389ffdb7186ea4339849142bb47a65537
Harshad <harshadarvindh@gmail.com>
2024-04-01 15:47:16 -0400

    Update and rename script.js to home.js

--- home.js
+++ home.js
@@ -1,3 +1,9 @@
+window.onload = function() {
+  if (!localStorage.getItem('redirected')) {
+    window.location.href = 'pages/educational_Portal.html';
+    localStorage.setItem('redirected', true);
+  }
+}
 
 
 
