cbc92bead3ed89c92f78ff1ee16efc0138134e0a
Harshad <harshadarvindh@gmail.com>
2024-04-01 16:37:52 -0400

    Update home.js

--- home.js
+++ home.js
@@ -4,39 +4,36 @@ window.onload = function (
     window.location.href = '/pages/educational_Portal.html';
     localStorage.setItem('redirected', true);
   }
-  
-  document.addEventListener("DOMContentLoaded", function (
-) {
-    const searchInput = document.getElementById("searchInput");
-    const gameContainer = document.getElementById("gameContainer");
-    const gameButtons = document.querySelectorAll(".Game_Button");
-    const splashScreen = document.getElementById("splashScreen");
-    const mainContent = document.getElementById("mainContent");
 
-    // Handle search input event
+  const searchInput = document.getElementById("searchInput");
+  const gameContainer = document.getElementById("gameContainer");
+  const gameButtons = document.querySelectorAll(".Game_Button");
+  const splashScreen = document.getElementById("splashScreen");
+  const mainContent = document.getElementById("mainContent");
+
+  // Handle search input event
 searchInput.addEventListener("input", function (
 ) {
-      const searchTerm = searchInput.value.trim().toLowerCase();
+    const searchTerm = searchInput.value.trim().toLowerCase();
 
-      gameButtons.forEach((button) => {
-        const gameName = button.querySelector("img").alt.toLowerCase();
-        if (gameName.includes(searchTerm)) {
-          button.style.display = "inline-block";
-        } else {
-          button.style.display = "none";
-        }
-      });
+    gameButtons.forEach((button) => {
+      const gameName = button.querySelector("img").alt.toLowerCase();
+      if (gameName.includes(searchTerm)) {
+        button.style.display = "inline-block";
+      } else {
+        button.style.display = "none";
+      }
     });
-
-    // Handle splash screen animation
-setTimeout(() => {
-      splashScreen.style.animation = "slidedown 0.9s ease-in-out forwards";
-
-      splashScreen.addEventListener("animationend", function (
-) {
-        splashScreen.style.display = "none";
-        mainContent.style.display = "block";
-      });
-    }, 1700);
   });
+
+  // Handle splash screen animation
+setTimeout(() => {
+    splashScreen.style.animation = "slidedown 0.9s ease-in-out forwards";
+
+    splashScreen.addEventListener("animationend", function (
+) {
+      splashScreen.style.display = "none";
+      mainContent.style.display = "block";
+    });
+  }, 1700);
 };
