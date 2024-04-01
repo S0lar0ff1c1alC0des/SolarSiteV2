df54f5ee8c6801f61b1b3527f4c89337332a5b39
Harshad <harshadarvindh@gmail.com>
2024-04-01 18:41:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+searchInput.addEventListener("input", function (
+) {
+        const searchTerm = searchInput.value.trim().toLowerCase();
+
+        gameButtons.forEach((button) => {
+            const gameName = button.querySelector("img").alt.toLowerCase();
+            if (gameName.includes(searchTerm)) {
+                button.style.display = "inline-block";
+            } else {
+                button.style.display = "none";
+            }
+        });
+    });
